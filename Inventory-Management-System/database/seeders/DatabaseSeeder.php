4bcc10d2257d067ff89e5a6ba0f27cb7458ac020
Talha <mahbuburrahmantalha@gmail.com>
2023-11-29 22:03:30 +0600

    seeder updated

--- Inventory-Management-System/database/seeders/DatabaseSeeder.php
+++ Inventory-Management-System/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
-                UsersSeeder::class
+                UsersSeeder::class,
+                ProductsSeeder::class
             ]
         );
     }
---
+++ Inventory-Management-System/database/seeders/ProductsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert(
+            [
+                'name' => 'TV',
+                'quantity' => 10,
+                'price' => 500000,
+            ]
+        );
+    }
+}
